a3455581172eb6b6cfb05129c08c32b845d167fd
hijal <hijikeshhijal@gmail.com>
2022-06-27 20:24:58 +0600

    es6 class added

--- ES6 Classes/Person.js
+++ ES6 Classes/Person.js
@@ -13,3 +13,23 @@ Person.prototype.getName = function () {
 
 const john = new Person('john doe');
 console.log(john.getName());
+
+console.log(john instanceof Person); // true
+console.log(john instanceof Object); // true
+
+/**
+ * Class ES6
+ */
+class Author {
+  constructor(name) {
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+}
+
+console.log(typeof Person); // function
+
+const author = new Author('John Doe');
+console.log('Author name is ', author.getName());
